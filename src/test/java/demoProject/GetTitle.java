29db0629c4b632f89c093bd6f2a1f570c9e9a517
Sandeshbe <104488145+Sandeshbe@users.noreply.github.com>
2023-01-30 12:05:03 +0530

    Update GetTitle.java

--- src/test/java/demoProject/GetTitle.java
+++ src/test/java/demoProject/GetTitle.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
